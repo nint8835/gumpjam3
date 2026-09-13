5292cae37136496d384ad7ba4de3a38e78e6f798
Riley Flynn <riley@rileyflynn.me>
2026-09-13 01:40:13 -0230

    Scaffold

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+export default {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrder: ['engine', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
