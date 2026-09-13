2513d3f00013b5e5a11b6e33df67e3174eeb565c
Riley Flynn <riley@rileyflynn.me>
2026-09-13 14:57:05 -0230

    Graphics

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -4,12 +4,17 @@ use uiua::*;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn test(code: String) -> Result<isize, JsError> {
+pub fn test(code: String) -> Result<Vec<f64>, JsError> {
     let mut uiua = Uiua::with_safe_sys();
 
     uiua.run_str(&code)?;
 
-    Ok(uiua.pop_int()?)
+    let image = uiua.pop("image")?;
+    let pixels = image
+        .as_num_array()
+        .ok_or_else(|| JsError::new("Expected a numeric image on the stack"))?;
+
+    Ok(pixels.data().to_vec())
 }
 
 #[wasm_bindgen]
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
 import { init, test } from 'engine';
 
+const canvas = document.getElementById('game') as HTMLCanvasElement;
+
 init();
 
-console.log(test('/+▽⊸(=0◿2)+1⇡10'));
+function render() {
+  const pixelArray = test(
+    `U ← /=⊞<⚂_⚂ /+×⟜ⁿ1_2
+I ← <⊙(⌵/ℂ) # Circle
+u ← +0.1⧋↧ ⊃(I0.95|⊂⊙0.5⇌˙×)
+A ← ×⊃U(I1) # Alpha
+⧋(⊂⊃u A) ˙⊞⊟-⊸¬÷⟜⇡200`,
+  );
+
+  const imageData = new ImageData(new Uint8ClampedArray(pixelArray.map((v) => v * 255)), 200, 200);
+
+  canvas.getContext('2d')?.putImageData(imageData, 0, 0);
+  requestAnimationFrame(render);
+}
+
+requestAnimationFrame(render);
