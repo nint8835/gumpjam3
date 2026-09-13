33572dd0e6bacb346ebc0800a8413a089b1dfb32
Riley Flynn <riley@rileyflynn.me>
2026-09-13 02:38:34 -0230

    It works!

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,13 +1,18 @@
 mod utils;
 
+use uiua::*;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+pub fn test(code: String) -> Result<isize, JsError> {
+    let mut uiua = Uiua::with_safe_sys();
+
+    uiua.run_str(&code)?;
+
+    Ok(uiua.pop_int()?)
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, engine!");
+pub fn init() {
+    utils::set_panic_hook();
 }
---
+++ src/main.ts
@@ -0,0 +1,3 @@
+import { test } from 'engine';
+
+console.log(test('/+▽⊸(=0◿2)+1⇡10'));
