12cf97567f1d598bcc9bee683b06ca0b17be301a
Riley Flynn <riley@rileyflynn.me>
2026-09-13 15:07:34 -0230

    Dynamic dimensions

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,15 @@ function render() {
 I ← <⊙(⌵/ℂ) # Circle
 u ← +0.1⧋↧ ⊃(I0.95|⊂⊙0.5⇌˙×)
 A ← ×⊃U(I1) # Alpha
-⧋(⊂⊃u A) ˙⊞⊟-⊸¬÷⟜⇡200`,
+⧋(⊂⊃u A) ˙⊞⊟-⊸¬÷⟜⇡⌈×⚂ 200`,
   );
 
-  const imageData = new ImageData(new Uint8ClampedArray(pixelArray.map((v) => v * 255)), 200, 200);
+  const dimensions = Math.sqrt(pixelArray.length / 4);
 
+  const imageData = new ImageData(new Uint8ClampedArray(pixelArray.map((v) => v * 255)), dimensions, dimensions);
+
+  canvas.width = dimensions;
+  canvas.height = dimensions;
   canvas.getContext('2d')?.putImageData(imageData, 0, 0);
   requestAnimationFrame(render);
 }
